078d986eddf9354aa3e8e2128198579dc30e0cb3
Manita <manitasitaula08@gmail.com>
2026-01-08 18:03:10 +0545

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,7 @@ $username="root";
 $password="";
 $databasename="phpCrud";
 
-$con = new mysqli($servername, $username, $password, $databasename, 3307); // 3306 is default port number.. since I changed it so I'm writing here.
+$con = new mysqli($servername, $username, $password, $databasename); 
 
 if($con->connect_error){
     die("connection failed ". $con->connect_error);
